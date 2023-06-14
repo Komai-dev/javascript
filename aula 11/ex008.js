4de4936f0a507eddf95035bfb06e1b437697acec
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-06-13 22:40:55 -0300

    a

---
+++ aula 11/ex008.js
@@ -0,0 +1,10 @@
+var maiko = 42
+var velo = maiko
+
+console.log(`A velocidade do seu carro é ${velo}Km/h.`)
+
+if (velo <= 45) {
+  console.log('Dirija sempre usando cinto de segurança')
+} else {
+  console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
