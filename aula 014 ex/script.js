a307175febe698b3d58c9bdd0aff2d31fb8338ac
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-07-01 00:08:19 -0300

    fiz com o guanabaram ;-;

--- aula 014 ex/script.js
+++ aula 014 ex/script.js
@@ -1,4 +1,37 @@
-function clicar() {
+function contar() {
+  let ini = document.getElementById('txti')
+  let fim = document.getElementById('txtf')
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('res')
+
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossível contar.'
+    alert('[Erro] Faltam dados!')
+  } else {
+    res.innerHTML = 'Contando: <br>'
+    let i = Number.parseInt(ini.value)
+    let f = Number.parseInt(fim.value)
+    let p = Number.parseInt(passo.value)
+
+    if (p <= 0) {
+      window.alert('Passo inválido! Considerando a partir de 1')
+      p = 1
+    }
+
+    if (i < f) {
+      for (let c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c} 👉 `
+      }
+    } else {
+      for (let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} 👉 `
+      }
+    }
+  }
+  res.innerHTML += `\u{1F3C1}`
+}
+
+/*function clicou() {
   var entrada = document.getElementById('inic')
   var inic = Number.parseInt(entrada.value)
   
@@ -11,8 +44,8 @@ function clicar() {
   var calc = inic + passo
   var result = document.querySelector('p#resultado')
 
-  for (soma = inic ; calc <= fim ; inic + passo) {
+  for (var soma = inic ; calc <= fim ; inic + passo) {
     result.innerHTML = `Contando: <br> 
      ${soma} 👉 ${calc}`
   }
-}
+}*/
